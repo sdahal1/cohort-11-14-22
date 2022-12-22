e660f700276195064a93cc1139d89e7022ac6e24
sdahal1 <raabdahl@gmail.com>
2022-12-22 08:34:05 -0800

    w5d4am

---
+++ Module-20-javascript-and-the-dom/20.4-updating-the-dom-platform/starter-national-parks/index.js
@@ -0,0 +1,76 @@
+//select all the elements with the class name of description-display
+let allDescriptions = document.querySelectorAll(".description-display")
+
+
+// console.log(allDescriptions);
+
+
+// for(let i = 0; i<allDescriptions.length; i++){
+//     let descriptionDiv = allDescriptions[i];
+//     // descriptionDiv.innerText = "huh who dis."
+// }
+
+
+allDescriptions.forEach((descriptionDiv)=>{
+    // console.log(descriptionDiv.innerText);
+    //if the descriptionDiv innertext has more than 250 characters, then trim the inner text to only be 250 characters
+    if(descriptionDiv.innerText.length > 250){
+        //trim
+        let shortenedDescription = descriptionDiv.innerText.slice(0,250)
+
+        //update the descriptionDiv witht he trimmed version
+        descriptionDiv.innerHTML = shortenedDescription + '<a href="#">...</a>';
+    }
+})
+
+
+//select all the ratings
+let ratingsDivs = document.querySelectorAll(".rating-display .value");
+// console.log(ratingsDivs);
+//loop through the list of ratings
+ratingsDivs.forEach((ratingDiv)=>{
+    //for each rating in the list, IF the rating is greater than 4.7, then make the rating be larger font size
+    // console.log(parseFloat(ratingDiv.innerText))
+    if(parseFloat(ratingDiv.innerText) > 4.7){
+        //modify the ratingDiv's css 
+        // ratingDiv.style.fontSize = "40px";
+        // // ratingDiv.style.textDecoration = "underline";
+        // ratingDiv.style.backgroundColor = "blue";
+        // ratingDiv.style.color = "white";
+        // ratingDiv.style.textDecoration = "line-through";
+
+        ratingDiv.classList.add("high-rating")
+        ratingDiv.classList.remove("value");
+    }
+})
+
+/* INSERTING ELEMENTS ON THE PAGE */
+
+//select all elements with the classname of park-display
+const parks = document.querySelectorAll(".park-display");
+// console.log(parks.length);
+const numberOfParks = parks.length;
+
+//create an element that is a div
+const newElement = document.createElement("div");
+console.log(newElement)
+
+//give the div some inner text (content)
+newElement.innerText = `There are ${numberOfParks} in this page.`
+newElement.classList.add("header-statement");
+//select where in the dom I want to insert this new div
+let header = document.querySelector("header");
+
+//insert the newElement into the dom;
+header.appendChild(newElement);
+
+
+/* REMOVING ELEMENTS FROM THE PAGE */
+
+//select the element i want to remove a child FROM
+let mainElement = document.querySelector("main");
+
+//select the element I want to remove
+let firstSection = document.querySelector(".park-display")
+
+mainElement.removeChild(firstSection);
---
+++ Module-20-javascript-and-the-dom/20.4-updating-the-dom-study-guide/index.js
@@ -0,0 +1,32 @@
+/* 
+1. addHeadingsAndImages-> make all <article>Puppy</article> look like 
+        <article>
+            <h3>Puppy</h3>
+            <img src = "linkgoeshere">
+        </article>
+2. styleKittensandPuppies-> style all cats and dogs by adding the classlist "kitten" or "puppy" to the article containing either a kitten or a puppy
+    eg: <article><h3>Puppy</h3></article> will look like <article class="puppy"><h3>Puppy 1</h3></article> and <article class="kitten"><h3>Kitten 1</h3></article>
+3. separateCatsFromDogs
+    -create a "kittens" section with the class of "kittens" to put kitten articles into
+    -if the article has a kitten, remove it from that section and add it to a new section for kittens
+*/
+
+
+
+let images = [
+    "https://media.tenor.com/BlHLysXBit4AAAAC/puppy-love-hi.gif",
+    "https://media.tenor.com/PTBNHIGHS-kAAAAd/dog-smile.gif",
+    "https://media.tenor.com/bK1qpWGyQKkAAAAM/kitty.gif",
+    "https://media.tenor.com/avkEJ6QsXLcAAAAM/puppy.gif",
+    "https://media.tenor.com/XybizgnL1zQAAAAM/kittens-cute.gif",
+    "https://media.tenor.com/gZHJZ3gNDYwAAAAM/cute-cat.gif",
+    "https://media.tenor.com/JMv_beVhW98AAAAM/perrete.gif",
+    "https://media.tenor.com/g9bkJfFtovMAAAAM/dog.gif",
+    "https://media.tenor.com/kKvpaWwGoPcAAAAM/stretch-kitty.gif",
+    "https://media.tenor.com/WT7snNMnZoMAAAAM/luv-you-cute-kitten.gif"
+];
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~solutions~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+
+
